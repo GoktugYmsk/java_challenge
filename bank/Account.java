4d8f35c1043de67144d6ebf2e16706ab5b4fadfc
GoktugYmsk <gogtug033@outlook.com>
2026-01-10 20:35:54 +0300

    first commit

---
+++ bank/Account.java
@@ -0,0 +1,29 @@
+public class Account {
+    private double balance;
+
+    public Account(double gktInitialBalance) {
+        this.balance = gktInitialBalance;
+    }
+
+    public void deposit(double miktar) {
+        if (miktar > 0) {
+            balance += miktar;
+            System.out.println("Para yatırıldı. Yeni bakiye: " + balance);
+        } else {
+            System.out.println("Geçersiz tutar!");
+        }
+    }
+
+    public void withdraw(double miktar) {
+        if (miktar > 0 && miktar <= balance) {
+            balance -= miktar;
+            System.out.println("Para çekildi. Yeni bakiye: " + balance);
+        } else {
+            System.out.println("Yetersiz bakiye veya geçersiz tutar!");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
---
+++ bank/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner gktScanner = new Scanner(System.in);
+        Account account = new Account(1000);
+
+        while (true) {
+            System.out.println("\n--- BANKA MENÜSÜ ---");
+            System.out.println("1 - Para Yatır");
+            System.out.println("2 - Para Çek");
+            System.out.println("3 - Bakiye Görüntüle");
+            System.out.println("4 - Çıkış");
+            System.out.print("Seçiminiz: ");
+
+            int choice = gktScanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Yatırılacak tutar: ");
+                    double depositAmount = gktScanner.nextDouble();
+                    account.deposit(depositAmount);
+                    break;
+
+                case 2:
+                    System.out.print("Çekilecek tutar: ");
+                    double withdrawAmount = gktScanner.nextDouble();
+                    account.withdraw(withdrawAmount);
+                    break;
+
+                case 3:
+                    System.out.println("Mevcut bakiye: " + account.getBalance());
+                    break;
+
+                case 4:
+                    System.out.println("Çıkış yapılıyor...");
+                    gktScanner.close();
+                    return;
+
+                default:
+                    System.out.println("Geçersiz seçim!");
+            }
+        }
+    }
+}
